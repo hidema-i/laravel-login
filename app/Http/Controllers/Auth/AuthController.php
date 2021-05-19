2a91c015f698fb80bd9f6227edf90bc354e70797
ShuumaIwamoto <iwa06ki12da@gmail.com>
2021-05-19 20:07:45 +0900

    ロジックの分離。機能完成

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,35 +6,64 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
     /**
      * @return View
      */
-
     public function showLogin()
     {
         return view('login.login_form');
     }
 
     /**
-     * @param App\Http\Requests\LoginFormRequest;
+     * @param App\Http\Requests\LoginFormRequest $request
+     * @return
      */
     public function login(LoginFormRequest $request)
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        // ①アカウントがロックされていたら弾く
+        $user = $this->user->getUserByEmail($credentials['email']);
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました');
-        } else {
-            return back()->withErrors([
-                'danger' => 'email or password error',
-            ]);
+        if (!is_null($user)) {
+            if ($this->user->isAccountLocked($user)) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                // ②成功したらエラーカウントを0にする
+                $this->user->resetErrorCount($user);
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+    
+            // ③ログイン失敗したらエラーカウントを1増やす
+            $user->error_count = $this->user->addErrorCount($user->error_count);
+            // ④エラーカウントが6以上の場合はアカウントをロックする
+            if ($this->user->lockAccount($user)) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営者に連絡してください。',
+                ]);
+            }
+            $user->save();
         }
+        
+        return back()->withErrors([
+            'danger' => 'メールアドレスかパスワードが間違っています。',
+        ]);
     }
+
     /**
      * ユーザーをアプリケーションからログアウトさせる
      *
@@ -50,5 +79,5 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
 
         return redirect()->route('login.show')->with('danger', 'ログアウトしました！');
-    }
+    } 
 }
